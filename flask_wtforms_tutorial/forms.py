dc18101981406bebf238816e700bbd75bb3fa1b7
Hamilton <hamilka@americas.corpdir.net>
2021-07-01 15:00:10 -0500

    adding initial admin portal and new question types email and phone

--- flask_wtforms_tutorial/forms.py
+++ flask_wtforms_tutorial/forms.py
@@ -11,14 +11,24 @@ from wtforms import (
     FormField,
 )
 from wtforms.validators import URL, DataRequired, Email, EqualTo, Length
+from wtforms.fields.html5 import EmailField, TelField
+
     
 class TextForm(FlaskForm):
     text = StringField("Placeholder")
     
 class SelectForm(FlaskForm):
     select = SelectField("Placeholder", choices=[])
+    
+class EmailForm(FlaskForm):
+    email = EmailField("Placeholder")
+    
+class PhoneForm(FlaskForm):
+    phone = TelField("Placeholder")
 
 class CompleteForm(FlaskForm):
     text_entries = FieldList(FormField(TextForm))
     select_entries = FieldList(FormField(SelectForm))
+    email_entries = FieldList(FormField(EmailForm))
+    phone_entries = FieldList(FormField(PhoneForm))
     submit = SubmitField("Submit")
--- flask_wtforms_tutorial/routes.py
+++ flask_wtforms_tutorial/routes.py
@@ -7,8 +7,8 @@ import os
 import os.path
 import uuid
 from werkzeug.utils import secure_filename
-from .forms import SelectForm, TextForm, CompleteForm
-
+from .forms import SelectForm, TextForm, EmailForm, PhoneForm, CompleteForm
+from datetime import datetime
 from flask import send_from_directory, abort
 
 # DATA_DIRECTORY = os.getenv('DATA_DIRECTORY')
@@ -16,6 +16,8 @@ DATA_DIRECTORY = "C:/Users/hamilka/Downloads/flask-wtforms-survey/Data/"
 SURVEY_DIRECTORY = os.path.join(DATA_DIRECTORY, "surveys/")
 RESPONSE_DIRECTORY = os.path.join(DATA_DIRECTORY, "results/")
 TEMP_DIRECTORY = os.path.join(DATA_DIRECTORY, "temp/")
+
+API_KEY = 123
 
 ALLOWED_EXTENSIONS = {'json'}
 
@@ -42,31 +44,75 @@ def get_custom_questions(file):
         data = json.load(f)
     return data
 
+# Get phone questions
+def get_phone_data_from_custom_questions(file):
+    type = "phone"
+    phone_data = []
+    custom_questions = get_custom_questions(file)
+    for field in custom_questions["fields"]:
+        if field["type"] == type:
+            phone_data.append(field["key"])
+    return phone_data
+
+# Format phone questions for survey
+def get_phone_entries(file):
+    phone_data = get_phone_data_from_custom_questions(file)
+    all_phone_items = []
+    for item in phone_data:
+        phone_id = uuid.uuid1()
+        phone_entry = PhoneForm()
+        phone_entry.phone.label = item
+        phone_entry.phone.name = item
+        phone_entry.phone.id = phone_id
+        all_phone_items.append(phone_entry)
+    return all_phone_items
+
+# Get email questions
+def get_email_data_from_custom_questions(file):
+    type = "email"
+    email_data = []
+    custom_questions = get_custom_questions(file)
+    for field in custom_questions["fields"]:
+        if field["type"] == type:
+            email_data.append(field["key"])  
+    return email_data
+
+# Format email questions for survey
+def get_email_entries(file):
+    email_data = get_email_data_from_custom_questions(file)
+    all_email_items = []
+    for item in email_data:
+        email_id = uuid.uuid1()
+        email_entry = EmailForm()
+        email_entry.email.label = item
+        email_entry.email.name = item
+        email_entry.email.id = email_id
+        all_email_items.append(email_entry)
+    return all_email_items
+
 # Get text questions
 def get_text_data_from_custom_questions(file):
     type = "text"
     text_data = []
-    custom_questions = get_custom_questions(file)
-    
+    custom_questions = get_custom_questions(file) 
     for field in custom_questions["fields"]:
         if field["type"] == type:
-            text_data.append(field["key"])
-            
+            text_data.append(field["key"])       
     return text_data
 
 # Format text questions for survey
 def get_text_entries(file):
     text_data = get_text_data_from_custom_questions(file)
-    
     all_text_items = []
     for item in text_data:
         text_id = uuid.uuid1()
         text_entry = TextForm()
+        # TO DO: Finish refactoring code to allow required flag to be set
+        # text_entry.text.flags.required = True
         text_entry.text.label = item
         text_entry.text.name = item
         text_entry.text.id = text_id
-        all_text_items.append(text_entry)
-        
+        all_text_items.append(text_entry)    
     return all_text_items
 
 # Get select questions
@@ -74,17 +120,14 @@ def get_select_data_from_custom_questions(file):
     type = "select"
     select_data = []
     custom_questions = get_custom_questions(file)
-
     for field in custom_questions["fields"]:
         if field["type"] == type:
             select_data.append({field["key"]: field["data_list"]})
-
     return select_data
 
 # Format select questions for survey
 def get_select_entries(file):
     select_data = get_select_data_from_custom_questions(file)
-
     all_select_items = []
     for select_dict in select_data:
         for k, v in select_dict.items():
@@ -95,7 +138,6 @@ def get_select_entries(file):
             select_entry.id = select_id
             select_entry.select.choices = v
             all_select_items.append(select_entry)
-
     return all_select_items
 
 # Get survey title    
@@ -115,9 +157,11 @@ def create_survey(name=None):
         form = CompleteForm()
         form.select_entries = get_select_entries(file)
         form.text_entries = get_text_entries(file)
-        title = get_survey_title(file)
+        form.email_entries = get_email_entries(file)
+        form.phone_entries = get_phone_entries(file)
         
-        # TO DO: Additional question types ie email, add field required option
+        title = get_survey_title(file)
+
         # TO DO: Question Priority
         # TO DO: Multiselect Questions
         # TO DO: Flag for required response
@@ -127,8 +171,12 @@ def create_survey(name=None):
                 with open(RESPONSE_DIRECTORY + name + '_results.json') as f:    
                     data = json.load(f)
                     
+                # add new survey response data and date
                 response = request.form.to_dict()
-                data[response.pop('csrf_token')] = response
+                new_key = response.pop('csrf_token')
+                data[new_key] = response
+                now = datetime.now()
+                data[new_key]['date'] = now.strftime("%m/%d/%y")
                 
                 with open(RESPONSE_DIRECTORY + name + '_results.json', 'w') as f:
                     json.dump(data, f)
@@ -182,7 +230,7 @@ def convert_csv(file):
 @app.route('/get-files/<int:key>/<path:path>',methods = ['GET','POST'])
 def download_file(key=None, path=None):
 
-    if key == 123:
+    if key == API_KEY:
         if '.csv' in path:
             convert_csv(path)
     
@@ -199,12 +247,12 @@ def download_file(key=None, path=None):
 End Download File
 """
 
-
+# TO DO: delete data?
 # Delete survey file
 @app.route('/delete/<int:key>/<string:name>', methods=["GET", "POST"])
 def delete_file(key=None, name=None):
     survey = name + '.json'
-    if key == 123:
+    if key == API_KEY:
         if request.method == "POST":
             if 'Cancel' in request.form:
                 return redirect(url_for("survey_home"))
@@ -232,6 +280,7 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 # Upload file
+# TO DO: Check if new survey is loadable i.e. json is loadable
 @app.route('/upload', methods=['GET','POST'])
 def upload_file():
     if request.method == 'POST':
@@ -285,6 +334,38 @@ def update_file(file):
 """
 End Upload Survey File
 """
+
+def get_entry_count(file):
+    f = open(os.path.join(RESPONSE_DIRECTORY, file))
+    responses = json.load(f)
+    return len(responses)
+
+def get_response_date(file):
+    f = open(os.path.join(RESPONSE_DIRECTORY, file))
+    responses = json.load(f)
+    length = len(responses) 
+    keys = list(responses.keys())
+    if length > 0:
+        return responses[keys[length-1]]['date']
+    else:
+        return 'N/A'
+
+# TO DO: Figure out basic authorization              
+@app.route("/admin", methods=["GET", "POST"])
+def admin():
+    surveys = []
+    for file in os.listdir(SURVEY_DIRECTORY):
+        results = file.replace(".json", "_results.json")
+        surveys.append([file.replace('.json', ''), 
+                        get_survey_title(file), 
+                        get_entry_count(results),
+                        get_response_date(results)])
+    return render_template(
+        "admin.jinja2",
+        title="Admin Homepage",
+        surveys=surveys,
+        key=API_KEY
+    )
 
 
 """
