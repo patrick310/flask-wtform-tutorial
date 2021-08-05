454e73ee308a8385a16b7add441b9f6594dcd10a
kchamilton <38380162+kchamilton@users.noreply.github.com>
2021-08-05 14:30:57 -0500

    fixes for checkbox format

--- mbusi_surveys/surveys/surveys.py
+++ mbusi_surveys/surveys/surveys.py
@@ -7,9 +7,14 @@ from datetime import datetime
 from flask import abort
 from flask import Blueprint
 from . import build_survey as build
+from dotenv import load_dotenv
+from wtforms.widgets import html_params
+
+load_dotenv()
 
 # DATA_DIRECTORY = os.getenv('DATA_DIRECTORY')
-DATA_DIRECTORY = "C:/Users/hamilka/Downloads/mbusi_surveys/data/"
+# DATA_DIRECTORY = "C:/Users/hamilka/Downloads/mbusi_surveys/data/"
+DATA_DIRECTORY = os.environ.get("DATA_DIRECTORY")
 SURVEY_DIRECTORY = os.path.join(DATA_DIRECTORY, "surveys/")
 RESPONSE_DIRECTORY = os.path.join(DATA_DIRECTORY, "responses/")
 
@@ -19,6 +24,27 @@ survey_bp = Blueprint(
     template_folder='templates',
     static_folder='static'
 )
+
+# Format checkboxes for multi select option
+@survey_bp.context_processor
+def utility_processor():
+    def select_multi_checkbox(field, ul_class='', **kwargs):
+        kwargs.setdefault('type', 'checkbox')
+        field_id = kwargs.pop('id', field.id)
+        html = [u'<ul %s>' % html_params(id=field_id, class_=ul_class)]
+        for value, label, checked in field.iter_choices():
+            choice_id = u'%s-%s' % (field_id, value)
+            options = dict(kwargs, name=field.name, value=value, id=choice_id)
+            if checked:
+                options['checked'] = 'checked'
+            html.append(u'<li style="overflow:hidden">')
+            html.append(u'<input style="float:left; width:10px" %s /> ' % html_params(**options))
+            html.append(u'<label style="float:left" for="%s">%s</label>' % (field_id, label))
+            html.append(u'</li>')
+        html.append(u'</ul>')
+        
+        return u''.join(html)
+    return dict(select_multi_checkbox=select_multi_checkbox)
 
 # Survey home page
 @survey_bp.route('/')
@@ -43,7 +69,6 @@ def create_survey(name=None):
         form.text_entries = build.get_text_entries(file)
         form.email_entries = build.get_email_entries(file)
         form.phone_entries = build.get_phone_entries(file)
-        # TO DO: Fix multiselect display
         form.multi_entries = build.get_multi_entries(file)
         
         title = build.get_survey_title(file)
@@ -54,9 +79,6 @@ def create_survey(name=None):
         size = size + form.email_entries.__len__()
         size = size + form.phone_entries.__len__()
         size = size + form.multi_entries.__len__()
-
-        # TO DO: Question Priority
-        # TO DO: Flag for required response
         
         if form.validate_on_submit():
             if request.method == 'POST':
@@ -64,20 +86,17 @@ def create_survey(name=None):
                     data = json.load(f)
                     
                 # add new survey response data and date
-                # TO DO: Figure out how to get all multiselect selections
-                response = request.form.to_dict()
-                #response = request.form.getlist()
-                
+                response = request.form.to_dict()                
                 for multi_entry in form.multi_entries:
                     values = request.form.getlist(multi_entry.multi.name)
                     response[multi_entry.multi.name] = values
                     
-    
                 new_key = response.pop('csrf_token')
                 data[new_key] = response
                 now = datetime.now()
                 data[new_key]['date'] = now.strftime("%m/%d/%y")
                 
+                # TO DO: Save data to sqlite database instead of json file
                 with open(RESPONSE_DIRECTORY + name + '_responses.json', 'w') as f:
                     json.dump(data, f)
             return redirect(url_for("survey_bp.submitted"))
