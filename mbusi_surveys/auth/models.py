1c09751fb1e186c32d7cd510ddbc71272c702d23
Patrick Weber <patrick310@gmail.com>
2021-08-12 14:18:13 -0500

    Update models.py

--- mbusi_surveys/auth/models.py
+++ mbusi_surveys/auth/models.py
@@ -3,7 +3,7 @@ from .. import db
 
 class User(db.Model, UserMixin):
     __tablename__ = 'user'
-    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    id = db.Column(db.Integer, primary_key=True, unique=True) # primary keys are required by SQLAlchemy
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
