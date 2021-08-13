bded4f188cdea5bb63c3e2ecfdd79adaa329dd48
Patrick Weber <patrick310@gmail.com>
2021-08-13 15:00:55 -0500

    Update admin.py

--- mbusi_surveys/admin/admin.py
+++ mbusi_surveys/admin/admin.py
@@ -249,18 +249,17 @@ def get_survey_title(file):
 @login_required
 def admin():
     surveys = []
-    try:
-        for file in os.listdir(SURVEY_DIRECTORY):
-            responses = file.replace(".json", "_responses.json")
-            try:
-                surveys.append([file.replace('.json', ''), 
-                                get_survey_title(file), 
-                                get_entry_count(responses),
-                                get_response_date(responses)])
-            except OSError or IOError as e:
-                print(e)
-    except OSError or IOError as e:
-        print("No Surveys")
+    for file in os.listdir(SURVEY_DIRECTORY):
+        responses = file.replace(".json", "_responses.json")
+        try:
+            surveys.append([file.replace('.json', ''), 
+                            get_survey_title(file), 
+                            get_entry_count(responses),
+                            get_response_date(responses)])
+            print("Successfully appended " + get_survey_title(file) + " to the admin set")
+        except OSError or IOError as e:
+            print(e)
+            print("Missed adding a survey for a reason")
     return render_template(
         "admin.jinja2",
         title="Admin Homepage",
